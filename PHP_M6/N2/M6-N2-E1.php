39163c10e8a6ee94a3af8e7d0949e0c3302ca95b
enric <enric@labaseweb.com>
2021-06-28 15:42:38 +0200

    primer commit exercicis módul 6, nivell 2

---
+++ PHP_M6/N2/M6-N2-E1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function compta2en2(){
+                for($i=2;$i<=10;$i+=2){
+                    print_r($i." ");
+                }
+            }
+            compta2en2();
+        ?>
+    </body>
+</html>
---
+++ PHP_M6/N2/M6-N2-E2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function compta2en2(){
+                $fi=85;
+                for($i=2;$i<=$fi;$i+=2){
+                    print_r($i." ");
+                }
+            }
+            compta2en2();
+        ?>
+    </body>
+</html>
